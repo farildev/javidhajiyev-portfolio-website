7f5b7fe6b953d582e47dc9f34de0d2c3a6185417
farildev <memmedovfaril@gmail.com>
2022-10-02 12:58:19 +0400

    v1.0

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 AOS.init();
+
+$('.portfolio__carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    centerPadding: '25px',
+    arrows: false
+  });
