7506586e0d9001ac517f56648987a41ebaa1b522
farildev <memmedovfaril@gmail.com>
2022-10-14 23:07:54 +0400

    v1.0

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,15 +9,48 @@ window.addEventListener('scroll' , function(){
 //Slick Slider Zone
 
 $('.portfolio__carousel').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    centerPadding: '25px',
-    arrows: false,
-    dots:true,
-    autoplaySpeed:2000,
-    centerMode:true,
-  });
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  centerPadding: '10px',
+  arrows: false,
+  dots:true,
+  autoplaySpeed:2000,
+  centerMode:true,
+  responsive: [
+    {
+      breakpoint:1280,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        centerPadding: '10px',
+      },
+    },
+    {
+      breakpoint:992,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint:480,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        centerMode: true,
+        dots: false,
+      },
+    },
+  ]
+});
 
   AOS.init();
